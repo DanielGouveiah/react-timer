cb3e825a496e5844e565d327d8734dd293c237aa
danielgouveiah <nel_hue@hotmail.com>
2022-05-10 22:18:43 -0300

    building

--- src/Timer.js
+++ src/Timer.js
@@ -1,45 +1,75 @@
 import { useEffect, useState } from "react";
 
 const Timer = () => {
-  const [minutes, setMinutes] = useState(10);
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  let m = minutes;
+  const [isPending, setIsPending] = useState(false);
   let s = seconds;
+  let m = minutes;
+
   useEffect(() => {
-    if (minutes === 0) {
-      setMinutes("00");
-    } else if (seconds === 0) {
-      setSeconds("00");
+    if (s == 60) {
+      if (m < 10) {
+        setMinutes(`0${Number(m) + 1}`);
+      } else {
+        setMinutes(Number(m) + 1);
+      }
+      setSeconds(0);
+    } else if (s === 0) {
+      setSeconds(`0${s}`);
+    } else if (m === 0) {
+      setMinutes(`0${m}`);
     }
   });
 
-  const timerFn = () => {
-    if (s > 0) {
-      if (s > 10) {
-        --s;
+  const secondLess = () => {
+    if (s !== 0) {
+      setSeconds(s);
+      s -= 1;
+      if (s < 10) {
+        setSeconds(`0${s}`);
       } else {
-        s = `0${--s}`;
+        setSeconds(s);
       }
     } else {
       setSeconds(0);
-      if (m > 0) {
-        m--;
-        s = 59;
-        if (m < 10) {
-          m = `0${m}`;
-        }
-      } else {
-        m = 0;
-      }
-      setMinutes(m);
     }
-
-    return setSeconds(s);
   };
 
+  const minuteLess = () => {
+    if (m > 0) {
+      if (s <= 0) {
+        m -= 1;
+        s = 59;
+      }
+    }
+    setMinutes(m);
+  };
   const active = () => {
-    setInterval(timerFn, 1000);
+    var interval = setInterval(() => {
+      secondLess();
+      minuteLess();
+      if (s == 0 && m == 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+    setIsPending(true);
   };
+
+  const addTime = () => {
+    if (seconds === 0) {
+      setSeconds(10);
+    } else {
+      s = Number(seconds) + 10;
+      setSeconds(s);
+    }
+  };
+
+  const pause = () => {
+    setIsPending(false);
+  };
+
+  let btnTester = s == 0 && m == 0;
 
   return (
     <div>
@@ -48,9 +78,12 @@ const Timer = () => {
           <span>
             {minutes}:{seconds}
           </span>
-          <button onClick={active}></button>
+          {!isPending && <button className="play" onClick={active}></button>}
+          {btnTester && <button className="play stop"></button>}
+          {isPending && <button className="pause" onClick={pause}></button>}
         </div>
       </div>
+      <button onClick={addTime}>10+</button>
     </div>
   );
 };
